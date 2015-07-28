90ddb847e71243044e10cf488042670897c169af
fahad-gcet <fms525@outlook.com>
2015-07-28 22:33:28 +0530

    Linear Search Using Recursion

---
+++ Searching/linear_search_recursive.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int linear_search(int[],int,int,int);
+void main()
+{
+	int x[100],n,m,p,ctr,i;
+	printf("Enter number of elements in the array ");
+	scanf("%d",&n);
+	printf("Enter %d elements into the array ",n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&x[i]);
+	}
+	printf("Enter the element to search ");
+	scanf("%d",&m);
+	ctr=0;
+	p=linear_search(x,n,m,ctr);
+	if(p==0)
+	{
+		printf("Element not found\n");
+	}
+    else
+	{
+		printf("Element is present at postion %d\n",p);
+	}
+			
+}
+	int linear_search(int x[],int n,int m,int ctr)
+	{
+		if(ctr>=n)
+		{
+			return 0;
+		}
+		else if(x[ctr]==m)
+		{
+			return ctr+1;
+		}
+		else
+		{
+			linear_search(x,n,m,++ctr);
+		}			
+	}
