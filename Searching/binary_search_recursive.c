8c8b67b236cd30a4743cf2f475df34ad0e8c01ad
fahad-gcet <fms525@outlook.com>
2015-07-29 21:47:02 +0530

    Binary Search Using Recursion

---
+++ Searching/binary_search_recursive.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int binary_search (int x[],int,int,int,int);
+void main()
+{
+	int x[100],n,m,p,i,upper,lower;
+	printf("Enter the number of elements in the array ");
+	scanf("%d",&n);
+	printf("Enter %d elements into the array ",n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&x[i]);
+	}
+	printf("Enter the element to search in the array ");
+	scanf("%d",&m);
+	lower=0;
+	upper=n-1;
+	p=binary_search(x,n,m,lower,upper);
+	if(p==0)
+	{
+		printf("Element not found\n");
+	}
+	else
+	{
+		printf("Element found at position %d\n",p);
+	}
+}
+int binary_search(int x[],int n,int m,int lower,int upper)
+{
+	int mid=(lower+upper)/2;
+	if(lower>=upper)
+	{
+		return 0;
+	}
+	else if(x[mid]==m)
+	{
+		return mid+1;
+	}
+	else if(x[mid]>m)
+	{
+		binary_search(x,n,m,lower,mid-1);
+	}
+	else if(x[mid]<m)
+		binary_search(x,n,m,mid+1,upper);
+}
