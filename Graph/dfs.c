16c267aa97e06d3af2d7a5d7b80d2d26567a88a3
fahad-gcet <fms525@outlook.com>
2015-10-19 08:11:54 +0530

    dfs.c added

--- Graph/dfs.c
+++ Graph/dfs.c
@@ -1,44 +1,29 @@
 #include<stdio.h>
-int a[20][20],reach[20],n,s;
-void dfs(int v)
-{
- int i;
- reach[v]=1;
- for(i=1;i<=n;i++)
-  if(a[v][i] && !reach[i])
-  {
-   printf("\n%d->%d",v,i);
-   dfs(i);
-  }
-}
+void DFS(int);
+int G[10][10],visited[10],n,source;
 int main()
 {
-	int i,j,count=0;
+	int i,j;
 	printf("Enter number of vertices: ");
 	scanf("%d",&n);
-	for(i=1;i<=n;i++)
-	{
-		reach[i]=0;
-		for(j=1;j<=n;j++)
-			a[i][j]=0;
-	}
-	printf("Enter the adjacency matrix:\n");
-	for(i=1;i<=n;i++)
-		for(j=1;j<=n;j++)
-			scanf("%d",&a[i][j]);
-		printf("Enter the source of the graph: ");
-		scanf("%d",&s);
-		printf("The DFS traversal of graph is:");
-		dfs(s);
-		for(i=1;i<=n;i++)
-		{
-			if(reach[i])
-				count++;
-		}
-		if(count==n)
-			printf("\nGraph is connected\n");
-		else
-			printf("\nGraph is not connected\n");
-		return 0;
+	printf("Enter adjecency matrix of the graph:\n");
+	for(i=0;i<n;i++)
+		for(j=0;j<n;j++)
+			scanf("%d",&G[i][j]);
+	for(i=0;i<n;i++)
+		visited[i]=0;
+	printf("Enter the source of the graph: ");
+	printf("The DFS traversal of graph is: ");
+	scanf("%d",&source);
+	DFS(source);
+	return 0;
 }
-
+void DFS(int i)
+{
+	int j;
+	printf("%d ",i);
+	visited[i]=1;
+	for(j=0;j<n;j++)
+		if(!visited[j]&&G[i][j]==1)
+			DFS(j);
+}
